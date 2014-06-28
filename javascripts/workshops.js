01b2d1b1e523fbf9a59cd72d1b3eddff1dee5e00
Hannah Nordgren <hey@hereshannahs.info>
2014-06-28 12:07:45 -0400

    started workshops area

---
+++ javascripts/workshops.js
@@ -0,0 +1,12 @@
+  $(document).ready(function (){
+    console.log('workshops loaded');
+
+   var contentBox= $('.content-box-workshops').hide();
+
+    $('#pirate').click(function(){
+      console.log('pirate');
+
+
+    });
+
+  });
