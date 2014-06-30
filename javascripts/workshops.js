0cfa222d995cac31681f1d1aa67ca05c4aac72be
Hannah Nordgren <hey@hereshannahs.info>
2014-06-30 14:27:01 -0400

    got workshop boxes

--- javascripts/workshops.js
+++ javascripts/workshops.js
@@ -1,12 +1,25 @@
   $(document).ready(function (){
     console.log('workshops loaded');
 
-   var contentBox= $('.content-box-workshops').hide();
+    $('.content-box-pirate').hide();
+    $('.content-box-princess').hide();
+    $('.content-box-superhero').hide();
+
 
     $('#pirate').click(function(){
       console.log('pirate');
+      $('.content-box-pirate').fadeIn(600);
 
+    });
 
+    $('#princess').click(function(){
+      console.log('princess');
+      $('.content-box-princess').fadeIn(600);
+    });
+
+    $('#superhero').click(function(){
+      console.log('superhero');
+      $('.content-box-superhero').fadeIn(600);
     });
 
   });
