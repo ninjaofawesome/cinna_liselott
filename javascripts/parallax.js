c5a9fef3b326d51c956b8be4a7ce3c42c86897be
Hannah Nordgren <hey@hereshannahs.info>
2014-07-16 23:16:10 -0400

    added blog buttons

--- javascripts/parallax.js
+++ javascripts/parallax.js
@@ -1,42 +1,71 @@
   $(document).ready(function (){
 
-    $("#about").click(function (){
+$('button').click(function(){
+    if($('.btn').parent('#home-box')){
       $('html, body').animate({
+        if('#about'){
           scrollTop: $("#about-box").offset().top
-      }, 1000);
-    });
+        }
+        if('#about'){
+          scrollTop: $("#about-box").offset().top
+        }
 
-    $("#workshop").click(function (){
-      $('html, body').animate({
-          scrollTop: $("#workshop-box").offset().top
-      }, 1400);
-    });
+}
+
+});
+
+  // $(function() {
+  //   $('a[href*=#]:not([href=#])').click(function() {
+  //     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+  //       var target = $(this.hash);
+  //       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+  //       if (target.length) {
+  //         $('html,body').animate({
+  //           scrollTop: target.offset().top
+  //         }, 1000);
+  //         return false;
+  //       }
+  //     }
+  //   });
+  // });
+
+    // $("#about").click(function (){
+    //   $('html, body').animate({
+    //       scrollTop: $("#about-box").offset().top
+    //   }, 1000);
+    // });
+
+    // $("#workshop").click(function (){
+    //   $('html, body').animate({
+    //       scrollTop: $("#workshop-box").offset().top
+    //   }, 1400);
+    // });
 
 
-    $("#aboutHome").click(function (){
-      $('html, body').animate({
-          scrollTop: $(".home").offset().top
-      }, 1000);
-    });
+    // $("#aboutHome").click(function (){
+    //   $('html, body').animate({
+    //       scrollTop: $(".home").offset().top
+    //   }, 1000);
+    // });
 
 
-    $("#aboutWorkshop").click(function (){
-      $('html, body').animate({
-          scrollTop: $("#workshop-box").offset().top
-      }, 1000);
-    });
+    // $("#aboutWorkshop").click(function (){
+    //   $('html, body').animate({
+    //       scrollTop: $("#workshop-box").offset().top
+    //   }, 1000);
+    // });
 
-    $("#workshopHome").click(function (){
-      $('html, body').animate({
-          scrollTop: $(".home").offset().top
-      }, 1400);
-    });
+    // $("#workshopHome").click(function (){
+    //   $('html, body').animate({
+    //       scrollTop: $(".home").offset().top
+    //   }, 1400);
+    // });
 
-    $("#workshopAbout").click(function (){
-      $('html, body').animate({
-          scrollTop: $(".about-box").offset().top
-      }, 1000);
-    });
+    // $("#workshopAbout").click(function (){
+    //   $('html, body').animate({
+    //       scrollTop: $(".about-box").offset().top
+    //   }, 1000);
+    // });
 
 
 
