c974bfaa0d34977aa55d53bea1250c6851f9120d
Hannah Nordgren <hey@hereshannahs.info>
2014-07-20 23:32:06 -0400

    refactored parallax-like scroll

--- javascripts/parallax.js
+++ javascripts/parallax.js
@@ -1,74 +1,14 @@
-  $(document).ready(function (){
+$(document).ready(function (){
 
-// $('button').click(function(){
-//     if($('.btn').parent('#home-box')){
-//       $('html, body').animate({
-//         if('#about'){
-//           scrollTop: $("#about-box").offset().top
-//         }
-//         if('#about'){
-//           scrollTop: $("#about-box").offset().top
-//         }
+  $('button').click(function(){
+    var destination = this.id.split('-')[1];
+    console.log(destination);
+    var offset = $("." + destination + "-box").offset().top;
 
-// }
-
-// });
-
-  // $(function() {
-  //   $('a[href*=#]:not([href=#])').click(function() {
-  //     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-  //       var target = $(this.hash);
-  //       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-  //       if (target.length) {
-  //         $('html,body').animate({
-  //           scrollTop: target.offset().top
-  //         }, 1000);
-  //         return false;
-  //       }
-  //     }
-  //   });
-  // });
-
-
-//this works but not what I want, WIP.
-    $("#about").click(function (){
-      $('html, body').animate({
-          scrollTop: $("#about-box").offset().top
-      }, 1000);
-    });
-
-    $("#workshop").click(function (){
-      $('html, body').animate({
-          scrollTop: $("#workshop-box").offset().top
-      }, 1400);
-    });
-
-
-    $("#aboutHome").click(function (){
-      $('html, body').animate({
-          scrollTop: $(".home").offset().top
-      }, 1000);
-    });
-
-
-    $("#aboutWorkshop").click(function (){
-      $('html, body').animate({
-          scrollTop: $("#workshop-box").offset().top
-      }, 1000);
-    });
-
-    $("#workshopHome").click(function (){
-      $('html, body').animate({
-          scrollTop: $(".home").offset().top
-      }, 1400);
-    });
-
-    $("#workshopAbout").click(function (){
-      $('html, body').animate({
-          scrollTop: $(".about-box").offset().top
-      }, 1000);
-    });
-
-
+    $('html, body').animate({
+      scrollTop: offset
+    }, 1200);
 
   });
+
+});
